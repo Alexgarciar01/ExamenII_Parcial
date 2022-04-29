3dd84ba1a66a002afedc517fd75cd849cbb6e5fe
Alexgarciar01 <98776549+Alexgarciar01@users.noreply.github.com>
2022-04-29 02:50:46 -0600

    segundocommit
    segundocommit

---
+++ Examen_Golosinas/Datos/Accesos/UsuarioDA.cs
@@ -0,0 +1,25 @@
+﻿using Datos.Entidades;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Accesos
+{
+    public class UsuarioDA
+    {
+        readonly string cadena = "Server=localhost; Port=3306; Database=examengolosinas; Uid=root; Pwd=alfaete01;";
+
+
+        MySqlConfiguration conn;
+        MySqlCommand cmd;
+
+        public Usuario Login(string codigo, string clave)
+        {
+            Usuario user = null;
+        }
+
+    }
+}
--- Examen_Golosinas/Examen_Golosinas/Program.cs
+++ Examen_Golosinas/Examen_Golosinas/Program.cs
@@ -16,7 +16,7 @@ namespace Examen_Golosinas
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
